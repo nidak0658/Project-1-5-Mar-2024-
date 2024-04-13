723665674c4508fcccb08d715549ca80fa10d99e
nidak0658 <165899145+nidak0658@users.noreply.github.com>
2024-04-13 13:47:17 +0500

    Update main.ts

--- Practice 1/main.ts
+++ Practice 1/main.ts
@@ -1454,4 +1454,4 @@
 // traditionalVsArrow.traditionalFunction(); // Logs "traditionalVsArrow value"
 // traditionalVsArrow.arrowFunction(); // Likely undefined, depending on the outer scope's 'this.value'
 // This code snippet illustrates the difference in how 'this' is determined in traditional functions versus arrow functions.
-// ----------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------
